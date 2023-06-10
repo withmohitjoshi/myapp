0a5526dc6eaaedb04c2d1588c2155e90d97430bd
Mohit Joshi <withmohitjoshi@gmail.com>
2023-06-10 16:44:28 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,81 +1,64 @@
-import axios from 'axios';
-import './App.css';
-import SingleDrop from './components/SingleDrop';
-import { Controller, useForm } from 'react-hook-form';
+import axios from "axios";
+import "./App.css";
+import SingleDrop from "./components/SingleDrop";
+import { Controller, useForm } from "react-hook-form";
 
 const apiClient = axios.create({
   headers: {
-    'x-auth-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkaWFsemVyLWp3dCIsInN1YiI6eyJ1dWlkIjoiMzczMmExYzktMzZhMC00MmM4LWIwNjUtZjk2ODRmNTVlNTY4IiwiZGJfbmFtZSI6ImdwazMxOTgxNjMxXzEiLCJwYXJlbnRfaWQiOm51bGwsImVtYWlsIjoicGFua2FqLnNpbmdoQGNhcGFuaWN1cy5jb20iLCJkb21haW4iOiJncGszMTk4MTYzMS5kaWFsemVyLmNvbSIsInVzZXJfZXh0ZW5zaW9uIjoiNDQ4MSIsImZpcnN0bmFtZSI6IlBhbmthaiIsImxhc3RuYW1lIjoiU2luZ2giLCJ0eXBlIjoiVSIsImNhbGxlcl9pZCI6IisxNzYwOTkxNTkwMyIsImlkIjoyMDIsInRpbWV6b25lIjoiSVNUIiwicGxhbl91dWlkIjoiOTgzMzU2YmYtYzVjOS00MWRlLWE4ZjYtN2YyYjE1OGIyN2ZmIn0sImlhdCI6MTY4NjI4OTM4OCwiZXhwIjoxNjkxNDczMzg4fQ.W1RyLU1iUA68k_nVSpfUjmk0CMz7WV6JSvoCvVQiGPk'
-  }
-})
+    "x-auth-token":
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkaWFsemVyLWp3dCIsInN1YiI6eyJ1dWlkIjoiMzczMmExYzktMzZhMC00MmM4LWIwNjUtZjk2ODRmNTVlNTY4IiwiZGJfbmFtZSI6ImdwazMxOTgxNjMxXzEiLCJwYXJlbnRfaWQiOm51bGwsImVtYWlsIjoicGFua2FqLnNpbmdoQGNhcGFuaWN1cy5jb20iLCJkb21haW4iOiJncGszMTk4MTYzMS5kaWFsemVyLmNvbSIsInVzZXJfZXh0ZW5zaW9uIjoiNDQ4MSIsImZpcnN0bmFtZSI6IlBhbmthaiIsImxhc3RuYW1lIjoiU2luZ2giLCJ0eXBlIjoiVSIsImNhbGxlcl9pZCI6IisxNzYwOTkxNTkwMyIsImlkIjoyMDIsInRpbWV6b25lIjoiSVNUIiwicGxhbl91dWlkIjoiOTgzMzU2YmYtYzVjOS00MWRlLWE4ZjYtN2YyYjE1OGIyN2ZmIn0sImlhdCI6MTY4NjI4OTM4OCwiZXhwIjoxNjkxNDczMzg4fQ.W1RyLU1iUA68k_nVSpfUjmk0CMz7WV6JSvoCvVQiGPk",
+  },
+});
 
 export const list = (payload) => {
   return apiClient({
     baseURL: "https://tenant-api-dev.dialzer.com/api/",
     method: "POST",
     url: "phonebook/contact-list",
-    data: payload
-  })
-}
+    data: payload,
+  });
+};
 function App() {
   const { control, handleSubmit } = useForm({
     defaultValues: {
       username: "",
       listName: "",
-    }
-  })
+    },
+  });
 
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleSubmit((data) => {
-          console.log(data);
-        })}>
-          {/* <Controller
-            name='listName'
-            control={control}
-            render={({ field }) => {
-              return <SingleDrop
-                queryFn={list}
-                queryFnPayload={{
-                  group_uuid
-                    :
-                    "FCC81696-6F03-4688-BD61-ADFDCA00BE99",
-                }}
-                queryKey='list'
-                label='firstname'
-                select='uuid'
-                // controllerField={field}
-                onChange={(value) => {
-                  console.log(value);
-                }}
-              />
-            }}
-          /> */}
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
           <Controller
-            name='username'
+            name="username"
             control={control}
             render={({ field }) => {
-              return <SingleDrop
-                fieldName="username"
-                label='name'
-                select='name'
-                isApiPresent={false}
-                rawDataForDropdown={[
-                  { name: "mohit", id: 1 },
-                  { name: "rohit", id: 2 },
-                  { name: "neha", id: 3 },
-                  { name: "kapil", id: 4 },
-                ]}
-                controllerField={field}
-                onChange={(value) => {
-                  console.log(value);
-                }}
-              />
+              return (
+                <SingleDrop
+                  fieldName="username"
+                  initalValue="15"
+                  isApiPresent={false}
+                  onChange={(value) => console.log(value)}
+                  displayLabel="name"
+                  selectedValue="id"
+                  placeholder="select username"
+                  formHookField={field}
+                  rawData={[
+                    { name: "mohit", id: 12 },
+                    { name: "rohit", id: 13 },
+                    { name: "tohit", id: 14 },
+                    { name: "lohit", id: 15 },
+                  ]}
+                />
+              );
             }}
           />
-
-          <button type='submit'>Submit</button>
+          <button type="submit">Submit</button>
         </form>
       </header>
     </div>
--- src/components/SingleDrop.js
+++ src/components/SingleDrop.js
@@ -1,83 +1,52 @@
-import { useQuery } from '@tanstack/react-query'
-import React, { useEffect, useState } from 'react'
-const handleIsOpen = (isOpen, setIsOpen) => setIsOpen(!isOpen)
+import React, { useEffect } from "react";
+import { useState } from "react";
+
 const SingleDrop = ({
   fieldName = "",
-  queryKey = "",
-  queryFn,
-  queryFnPayload = {},
-  placeHolder = "Please select",
-  select = "",
-  label = "",
+  initalValue = "",
+  placeholder = "",
   isApiPresent = true,
-  rawDataForDropdown = [],
-  controllerField = {},
-  error = '',
-  onChange = () => { }
+  queryKey = "",
+  queryFn = () => {},
+  queryFnPayload = {},
+  selectedValue = "",
+  displayLabel = "",
+  rawData = [],
+  onChange = () => {},
+  formHookField = {},
 }) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const { data } = useQuery({
-    queryKey: [queryKey],
-    queryFn: () => queryFn({ page: 1, length: 10, ...queryFnPayload, }),
-    select: (data) => data?.data?.data,
-    enabled: isApiPresent
-  })
-
+  const [selectedOptionValue, setSelectedOptionValue] = useState(initalValue);
   useEffect(() => {
-    const body = document.getElementsByTagName('body')[0]
-    if (isOpen) {
-      body.addEventListener('click', () => handleIsOpen(isOpen, setIsOpen))
+    if (formHookField) {
+      formHookField.onChange(selectedOptionValue);
     }
-    return () => {
-      body.removeEventListener('click', () => handleIsOpen(isOpen, setIsOpen))
-    }
-  }, [isOpen])
+  }, [formHookField, onChange, selectedOptionValue]);
+  return (
+    <div className="dropdown-main">
+      <select
+        name={fieldName || queryKey}
+        className="dropdown-menu"
+        {...formHookField}
+        value={selectedOptionValue}
+        onChange={(e) => {
+          setSelectedOptionValue(e.target.value);
+          onChange(e.target.value);
+        }}
+      >
+        <option value={""}>{placeholder}</option>
+        {rawData.length === 0 && <option value={""}>Data not provided</option>}
+        {rawData.map((result, index) => {
+          const value = result[selectedValue];
+          const label = result[displayLabel];
+          return (
+            <option key={index} value={value}>
+              {label}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
 
-  if (isApiPresent) {
-    return (
-      <>
-        <select name={queryKey} {...controllerField} onChange={(e) => onChange(e.target.value)} onClick={() => setIsOpen(!isOpen)}>
-          {!data?.length && <option style={{ textAlign: "center" }}>No recored found</option>}
-          <option value={""}>{placeHolder}</option>
-          {data?.length > 0 && data.map((result, index) => {
-            const listValue = result[select]
-            const listLabel = result[label]
-            return <option value={listValue} key={index}>{listLabel}</option>
-          })}
-        </select>
-        {error && <span>{error}</span>}
-      </>
-    )
-  } else {
-    return (
-      <div className='dropdown-main' onClick={(e) => {
-        e.stopPropagation()
-      }}>
-        <div className='dropdown' onClick={() => handleIsOpen(isOpen, setIsOpen)}>
-          <p className='dropdown-placeholder'>{placeHolder}</p>
-        </div>
-
-        {isOpen &&
-          <ul className='dropdown-menu'>
-            <div className='dropdown-menu-search'>
-              <input type='search' />
-            </div>
-            {rawDataForDropdown.length === 0 ? <li>Data is not provided</li> :
-              <>
-                {rawDataForDropdown.map((result, index) => {
-                  const listValue = result[select]
-                  const listLabel = result[label]
-                  return <li {...controllerField} key={index} onClick={() => {
-                    onChange(listValue)
-                  }}>{listLabel}</li>
-                })}
-              </>
-            }
-          </ul>
-        }
-      </div>
-    )
-  }
-}
-
-export default SingleDrop
+export default SingleDrop;
